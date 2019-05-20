b6a7fbb9c2cc89a28e8065c53756b026c44f0da5
catalingriu <50585115+catalingriu@users.noreply.github.com>
2019-05-20 15:50:36 +0300

    sd

--- js/tabel.js
+++
@@ -1,6 +0,0 @@
-function arata(id) {
-	if(document.getElementById(id).style.display == "block")
-		document.getElementById(id).style.display = "none";
-	else
-		document.getElementById(id).style.display = "block";
-}
