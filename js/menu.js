b76e39ea51065deb254c74f232fd0846aa85ebef
catalingriu <50585115+catalingriu@users.noreply.github.com>
2019-05-20 21:12:54 +0300

    aaa

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,33 @@
 var stanga = "Principala1";
 var dreapta = "Principala2";
-r();
 
-function r() {
-  document.getElementById("Principala11").style.setProperty("fill", "rgba(255,255,255,0.2)", "important");
+function r()
+{
+  x = document.getElementsByClassName("selector");
+  for(var i = 0; i<x.length; i++)
+    x[i].style.fill= "rgba(255,255,255,0)";
+  document.getElementById(stanga+"1").style.fill = "rgba(255,255,255,0.15)";
+  document.getElementById(dreapta+"1").style.fill = "rgba(255,255,255,0.15)";
 }
-
 
 function st(id) {
   document.getElementById(stanga).style.display = "none";
-  document.getElementById(id).style.display = "block";
+  document.getElementById(id).style.display = "block";  
   stanga = id;
+  r();
 }
 
 function dr(id) {
   document.getElementById(dreapta).style.display = "none";
   document.getElementById(id).style.display = "block";
   dreapta = id;
+  r();
 }
 
 function f()
 {
   document.getElementById("OuterCircle").addEventListener('mouseleave', f3());
+  r();
 }
 
 function f2()
