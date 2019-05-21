5e9a9bb01c33f17ce80d84a0dd0f07237968853a
catalingriu <50585115+catalingriu@users.noreply.github.com>
2019-05-21 20:36:02 +0300

    aa

--- js/activ1.js
+++ js/activ1.js
@@ -1,19 +1,19 @@
-var slideIndex11 = 0;
-showSlides10();
+var slideIndex31 = 0;
+showSlides31();
 
-function showSlides11() {
+function showSlides31() {
   var i;
-  var slides = document.getElementsByClassName("mySlides11");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides31");
+  var dots = document.getElementsByClassName("dot31");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex11++;
-  if (slideIndex11 > slides.length) {slideIndex11 = 1}    
+  slideIndex31++;
+  if (slideIndex31 > slides.length) {slideIndex31 = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex11-1].style.display = "block";  
-  dots[slideIndex11-1].className += " active";
-  setTimeout(showSlides11, 2500); // Change image every 2 seconds
+  slides[slideIndex31-1].style.display = "block";  
+  dots[slideIndex31-1].className += " active";
+  setTimeout(showSlides31, 2500); // Change image every 2 seconds
 }
