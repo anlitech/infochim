16b430804818485e589001ab3853f30da31b48c3
catalingriu <50585115+catalingriu@users.noreply.github.com>
2019-05-21 19:38:49 +0300

    aaa

---
+++ js/activ1.js
@@ -0,0 +1,19 @@
+var slideIndex11 = 0;
+showSlides10();
+
+function showSlides11() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides11");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex11++;
+  if (slideIndex11 > slides.length) {slideIndex11 = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex11-1].style.display = "block";  
+  dots[slideIndex11-1].className += " active";
+  setTimeout(showSlides11, 2500); // Change image every 2 seconds
+}
